de9cb0e714884d5dce5554bcf5154f78f6de09e9
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-16 20:50:56 -0300

    Inicio de UI

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  parser: "babel-eslint",
+  rules: {},
+};
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import braintlyLogo from "./assets/braintlyLogo.jpeg";
+import "./App.scss";
+import Spinner from "./components/Spinner";
+import TaskList from "./components/TaskList";
+import { useContext } from "react";
+import { AppContext } from "./Provider/StateProvider";
+import AddTodoForm from "./components/AddTodoForm";
+
+function App() {
+  const { loadingTasks, priorities } = useContext(AppContext);
+  return (
+    <div className="container">
+      <img src={braintlyLogo} alt="" />
+      <h1>ToDo List - Braintly</h1>
+      <h3>Agregar tarea una tarea nueva</h3>
+      <AddTodoForm priorities={priorities} />
+      {loadingTasks ? <Spinner /> : <TaskList />}
+    </div>
+  );
+}
+
+export default App;
