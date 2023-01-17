7e83eb359cdf3def39974cf67a3f2e9d43aec89e
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-17 00:53:54 -0300

    Elementos para listar tareas

---
+++ src/components/Task.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import moment from "moment/moment";
+const STATUS_CLASS = {
+  baja: "bg-primary",
+  media: "bg-warning",
+  alta: "bg-danger",
+  default: "bg-secondary",
+};
+
+const Task = ({ task, setSelectTask }) => {
+  const priorityNameLowerCase = task.priority_name.toLowerCase();
+
+  const determinateBackgroundByPriorityName = (priorityName) => {
+    switch (priorityName) {
+      case "baja":
+        return STATUS_CLASS.baja;
+      case "media":
+        return STATUS_CLASS.media;
+      case "alta":
+        return STATUS_CLASS.alta;
+      default:
+        return STATUS_CLASS.default;
+    }
+  };
+
+  const priorityClass = determinateBackgroundByPriorityName(
+    priorityNameLowerCase
+  );
+
+  return (
+    <li className="list-group-item list-group-item-dark d-flex justify-content-start align-items-center">
+      <input
+        className="form-check-input me-0"
+        type="radio"
+        name="task"
+        value=""
+        id={task.id}
+        onClick={(evt) => {
+          setSelectTask(task);
+        }}
+      />
+      <label className="form-check-label me-auto ms-3" htmlFor={task.id}>
+        {task.title}
+      </label>
+      <span className={`badge ${priorityClass} rounded-pill ms-auto`}>
+        {task.priority_name} | Deadline:{" "}
+        {moment(task.due_date).format("DD/MM/YY HH:mm a")}
+      </span>
+    </li>
+  );
+};
+
+export default Task;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,51 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../Provider/StateProvider";
-
-const STATUS_CLASS = {
-  baja: "bg-primary",
-  media: "bg-warning",
-  alta: "bg-danger",
-  default: "bg-secondary",
-};
+import FormControls from "./FormControls";
+import Task from "./Task";
 
 const TaskList = () => {
-  const { tasks, errorTasks, priorities } = useContext(AppContext);
+  const { tasks } = useContext(AppContext);
+  const [tasksStore, setTasksStore] = useState([]);
+  const [selectTask, setSelectTask] = useState(null);
 
-  const determinateBackgroundByPriorityName = (priorityName) => {
-    switch (priorityName) {
-      case "baja":
-        return STATUS_CLASS.baja;
-      case "media":
-        return STATUS_CLASS.media;
-      case "alta":
-        return STATUS_CLASS.alta;
-      default:
-        return STATUS_CLASS.default;
-    }
+  useEffect(() => {
+    setTasksStore(tasks);
+  }, []);
+
+  const handleOrderByDueDate = (evt) => {
+    const tasksOrdered = tasksStore.sort((a, b) => {
+      return new Date(b.due_date) - new Date(a.due_date);
+    });
+
+    setTasksStore(tasksOrdered);
   };
 
   return (
     <div className="container">
-      <h3>Tareas:</h3>
-      <ul className="list-group">
-        {tasks.map((task) => {
-          const priorityNameLowerCase = task.priority_name.toLowerCase();
-          const priorityClass = determinateBackgroundByPriorityName(
-            priorityNameLowerCase
-          );
+      <div className="d-flex justify-content-between mb-3">
+        <h3>Tareas:</h3>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={handleOrderByDueDate}
+        >
+          Ordenar por deadline
+        </button>
+      </div>
 
-          return (
-            <li
-              key={task.id}
-              class="list-group-item list-group-item-dark d-flex justify-content-between align-items-center"
-            >
-              {task.title}
-              <span className={`badge ${priorityClass} rounded-pill`}>
-                {task.priority_name}
-              </span>
-            </li>
-          );
-        })}
+      {selectTask && <FormControls />}
+
+      <ul className="list-group">
+        {tasksStore.map((task) => (
+          <Task key={task.id} task={task} setSelectTask={setSelectTask} />
+        ))}
       </ul>
     </div>
   );
